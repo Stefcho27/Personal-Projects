4effe06f2842906bd0a6d29f494c7b977ad523cd
Stefcho27 <72232154+Stefcho27@users.noreply.github.com>
2022-06-25 17:08:24 +0300

    Create Problem 2.h

---
+++ InensiaProblems/Problem 2/Problem 2.h
@@ -0,0 +1,18 @@
+class Student {
+
+	string name;
+	unsigned int FacNum;
+	int grades[N];
+
+public:
+	Student();
+	void setName(string);
+	
+	string GetName();
+	unsigned int GetNum();
+
+	float getGPA();
+
+
+	friend istream & operator >>(istream&, Student&);
+}
